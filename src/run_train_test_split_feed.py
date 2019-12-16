adbd0a85fb9867989e96eae7d93716ddf7120d36
petersiemen <peter.siemen@googlemail.com>
2019-12-16 13:10:37 +0100

    split

--- src/run_train_test_split_feed.py
+++ src/run_train_test_split_feed.py
@@ -1,11 +1,7 @@
 import argparse
 import os
-import shutil
 import sys
-import json
-from tqdm import tqdm
 
-from datasets.detected_car_dataset import DetectedCarDataset
 from logging_config import *
 
 logger = logging.getLogger(__name__)
@@ -13,23 +9,32 @@ logger = logging.getLogger(__name__)
 HERE = os.path.dirname(os.path.realpath(__file__))
 
 import random
-
+import json
 random.seed(0)
 
 
-def split_into_train_and_test(in_file, split):
+def split_into_train_and_test(in_file, split, makes):
     assert os.path.exists(in_file), f"file {in_file} does not exist"
     train_file_path = os.path.join(os.path.dirname(in_file), "train.json")
     test_file_path = os.path.join(os.path.dirname(in_file), "test.json")
     assert os.path.exists(train_file_path) == False, f"{train_file_path} already exists."
     assert os.path.exists(test_file_path) == False, f"{test_file_path} already exists."
 
+    if makes is not None:
+        makes = set(makes)
+    logger.info(
+        f"splitting {in_file} into {split * 100} % test samples and {(1 - split) * 100} training samples. make filter: {makes}")
     with open(train_file_path, 'w') as train_file:
         with open(test_file_path, 'w') as test_file:
             with open(in_file) as f:
                 lines = f.readlines()
                 random.shuffle(lines)
                 for line in lines:
+                    if makes is not None:
+                        obj = json.loads(line)
+                        if obj['make'] not in makes:
+                            continue
+
                     if random.random() < split:
                         test_file.write(line)
                     else:
@@ -47,6 +52,12 @@ def run():
                         default=0.1,
                         help="train/test split. default 0.1")
 
+    parser.add_argument('-m', '--makes',
+                        dest='makes',
+                        type=str,
+                        default=None,
+                        help="makes to keep. if not present then we keep all makes")
+
     args = parser.parse_args()
     if args.in_file is None:
         parser.print_help()
@@ -54,8 +65,13 @@ def run():
     else:
         in_file = args.in_file
         split = args.split
+        makes = args.makes
+        if makes is not None:
+            with open(makes, 'r') as fp:
+                lines = fp.readlines()
+                makes = [l.strip() for l in lines if l]
 
-        split_into_train_and_test(in_file, split)
+        split_into_train_and_test(in_file, split, makes)
 
         sys.exit(0)
 
